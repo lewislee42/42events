b0920f92585a01473bf23e752353a096da06a663
Meng Seong Lee <melee@u81z03s03.42kl.edu.my>
2023-10-04 14:41:06 +0800

    fixed routing

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
-import Navbar from '../components/Navbar'
+import Navbar from '../../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
