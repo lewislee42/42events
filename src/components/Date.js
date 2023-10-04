f853c09ee7ee5db35d7fef1ba1b11525541cdb79
Yealing Tay <yetay@student.42kl.edu.my>
2023-10-04 16:38:10 +0800

    feat: /src/components/EventDataReader.js has the function to read all event data from the /data/events folder

---
+++ src/components/Date.js
@@ -0,0 +1,23 @@
+import { parseISO, format } from 'date-fns';
+
+/* Function takes a props consisting of: */
+/* 1. dateString: date/time string in ISO format */
+/* 2. want: the date/time format to return */
+/* and return the date/time in the format requested */
+export default function Date(props) {
+	const date = parseISO(props.dateString);
+	let fmt = 'LLLL d, yyyy, KK:mm b'
+	switch (props.want) {
+		case 'day':
+			fmt = 'd'
+			return <time dateTime={props.dateString}>{format(date, fmt)}</time>
+		case 'mon':
+			fmt = 'MMMM'
+			return <time dateTime={props.dateString}>{format(date, fmt)}</time>
+		case 'hmb':
+			fmt = 'KK:mm b'
+			return <time dateTime={props.dateString}>{format(date, fmt)}</time>
+		default:
+			return <time dateTime={props.dateString}>{format(date, fmt)}</time>
+	}
+}
