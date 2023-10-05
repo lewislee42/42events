24c17045d6a2a988f06d4d0792ff535856dda566
Yealing Tay <yetay@student.42kl.edu.my>
2023-10-05 17:40:15 +0800

    feat: Dashboard page has additional featured event card

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,23 @@
 import Head from 'next/head'
 import Navbar from '/src/components/Navbar'
 import { getSortedEvtsData } from '/src/components/EventDataReader';
+import DrawFeaturedEventCard from '/src/components/DrawFeaturedEventCard'
 import DrawEventCard from '/src/components/DrawEventCard'
 
 export async function getStaticProps() {
 	const EventsData = getSortedEvtsData({ path: '/data/events' });
+	const Featured = getSortedEvtsData({ path: '/data/featured' });
 	return {
 		props: {
 			EventsData,
+			Featured,
 		},
 	};
 }
 
-export default function Dashboard({ EventsData }) {
+export default function Dashboard({ EventsData, Featured }) {
+	const ftEvent = Featured[0]
+	console.log(ftEvent)
 	return (
 		<>
 			<Head>
@@ -20,6 +25,9 @@ export default function Dashboard({ EventsData }) {
 			</Head>
 			<Navbar />
 			<div className='p-8 sm:ml-64 pt-20 sm:pt-8 h-screen dark:bg-gray-900'>
+				<section id='featured'>
+					<DrawFeaturedEventCard props={ ftEvent } />
+				</section>
 				<section className='flex flex-wrap'>
 				{EventsData.map(({ id, title, type, when, where, bh, ep, ad }) => (
 					<DrawEventCard key={id} id={id} title={title} type={type} when={when} where={where} bh={bh} ep={ep} ad={ad} />
