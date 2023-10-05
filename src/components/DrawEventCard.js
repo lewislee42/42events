49173f450de68e2a9fc7dcbe4518c78eb920be19
Yealing Tay <yetay@student.42kl.edu.my>
2023-10-05 16:07:04 +0800

    UI update

--- src/components/DrawEventCard.js
+++ src/components/DrawEventCard.js
@@ -20,7 +20,7 @@ function evtColor({ type }) {
 }
 
 export default function DrawEventCard({ id, title, type, when, where, bh, ep, ad }) {
-	const ClassBox = 'w-[45%] min-w-[336px] max-w-[420px] ' +
+	const ClassBox = 'w-[45%] min-w-[280px] max-w-[420px] ' +
 		             'h-84 m-[8px] p-0 ' +
 		             'border-solid border-2 ' +
 		             'flex justify-normal'
@@ -36,7 +36,7 @@ export default function DrawEventCard({ id, title, type, when, where, bh, ep, ad
 		              'mt-0 mr-[4px] mb-0 ml-[4px] ' +
 		              'text-slate-700 dark:text-slate-300 ' +
 					  'text-[125%]'
-	const ClassReward = 'text-2xl'
+	const ClassReward = 'text-xl'
 	return (
 		<>
 			<div className={ClassBox}
