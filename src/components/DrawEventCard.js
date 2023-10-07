ae83d6133304aac29126a1719d56595127db2517
Yealing Tay <yetay@student.42kl.edu.my>
2023-10-07 11:07:55 +0800

    feat: Dark mode colours

--- src/components/DrawEventCard.js
+++ src/components/DrawEventCard.js
@@ -29,12 +29,12 @@ export default function DrawEventCard({ id, title, type, when, where, bh, ep, ad
 		              'text-white text-xs text-center align-middle'
 	const ClassDay = 'text-4xl font-bold'
 	const ClassTitle = 'm-0 px-2 ' +
-		               'text-slate-600 dark:text-slate-400 ' +
+		               'text-slate-600 ' +
 		               'uppercase font-bold'
-	const ClassDesc = 'px-[8px] text-sm text-black dark:text-white'
+	const ClassDesc = 'px-[8px] text-sm text-black dark:text-slate-600'
 	const ClassIcon = 'inline align-text-bottom ' +
 		              'mt-0 mr-[4px] mb-0 ml-[4px] ' +
-		              'text-slate-700 dark:text-slate-300 ' +
+		              'text-slate-700 dark:text-slate-600 ' +
 					  'text-[125%]'
 	const ClassReward = 'text-xl'
 	return (
